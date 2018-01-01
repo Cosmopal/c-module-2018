ef9c0825c6800bd6998b0772a2cd189bd2ba6b08
Palash Aggrawal <palash16064@iiitd.ac.in>
2018-01-01 13:04:13 +0530

    Attempted the tricky part.

--- lab3/add.s
+++ lab3/add.s
@@ -26,3 +26,25 @@ addl %edi, %esi
 movl %esi,%eax
 
 ret
+
+.size my_add, .-my_add
+.globl my_add_ptrs
+.type my_add_ptrs, @function
+
+my_add_ptrs:
+#pushq %rbp
+#movq %rsp, %rbp
+
+# movq %rdi, -12(%rbp)
+# movq %rsi, -20(%rbp)
+# movl -12(%rsp), %ebx
+# movl -20(%rsp), %eax
+# addl %ebx, eax
+
+# movq %rbp, %rsp
+# popq %rbp
+movl (%rsi), %edx
+movl (%rdi), %eax
+addl %edx, %eax
+
+ret
--- lab3/sum.c
+++ lab3/sum.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 extern int my_add(int a, int b);
-// extern int my_add_ptrs(int* a, int* b);
+extern int my_add_ptrs(int* a, int* b);
 
 int main(int argc, char const *argv[])
 {
@@ -11,6 +11,6 @@ int main(int argc, char const *argv[])
 	// int a4 = 1314;
 
 	printf("Sum is = %d\n", my_add(op1,op2));
-	// printf("Sum is = %d\n", my_add_ptrs(&op1,&op2));
+	printf("Sum is = %d\n", my_add_ptrs(&op1,&op2));
 	return 0;
 }
